54145a11f3852bc9497cf79bd6a8ed5a97e19426
Saurabh-66 <69436942+Saurabh-66@users.noreply.github.com>
2020-11-23 10:19:20 +0530

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -8,14 +8,14 @@ import 'firebase/auth';
 
 // Config details are deliberately removed for authentication to remain secure
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyB9EvCeloLB4gZFUTftyESZGIt2xybDvuA",
+    authDomain: "netflix-790d2.firebaseapp.com",
+    databaseURL: "https://netflix-790d2.firebaseio.com",
+    projectId: "netflix-790d2",
+    storageBucket: "netflix-790d2.appspot.com",
+    messagingSenderId: "699580678602",
+    appId: "1:699580678602:web:bcd01034a5c3d7755c4f33",
+    measurementId: "G-R8356X4SL1"
 };
 
 const firebase = Firebase.initializeApp(config);
